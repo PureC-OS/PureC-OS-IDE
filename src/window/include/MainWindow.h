7a907836add5e1226f58172f18532e4ade6f422d
pablaofficeal <pavlogorosko56@gmail.com>
2026-09-22 16:37:17 +0200

    start

--- src/window/include/MainWindow.h
+++ src/window/include/MainWindow.h
@@ -2,10 +2,6 @@
 
 #include <QMainWindow>
 
-class QTabWidget;
-class QTreeView;
-class QLabel;
-
 class MainWindow final : public QMainWindow {
     Q_OBJECT
 
@@ -17,19 +13,4 @@ public:
     MainWindow& operator=(const MainWindow&) = delete;
     MainWindow(MainWindow&&) = delete;
     MainWindow& operator=(MainWindow&&) = delete;
-
-private slots:
-    void onNewFile();
-    void onOpenFile();
-    void onAbout();
-
-private:
-    void setupMenuBar();
-    void setupCentralWidget();
-    void setupDocks();
-    void setupStatusBar();
-
-    QTabWidget* m_tabs{nullptr};
-    QTreeView* m_projectView{nullptr};
-    QLabel* m_statusLabel{nullptr};
 };
